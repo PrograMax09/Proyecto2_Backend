2fc026c00255508eaae5feefbb18cfb461bf3f0b
PrograMax09 <PrograMax09@gmail.com>
2024-05-06 20:58:52 +0100

    problem with getAllForums

--- api/routes/forum.router.js
+++ api/routes/forum.router.js
@@ -12,7 +12,7 @@ const {
   } = require('../middlewares/index.js')
 
   router.post('/', checkAuth, checkAdmin, createForum)
-  router.get('/', checkAuth, getAllForums)
+  router.get('/', /*checkAuth, */getAllForums)
   router.get('/:topic', checkAuth, getOneForum)
   router.patch('/:topic', checkAuth, checkAdmin, updateOneForum)
   router.delete('/:topic', checkAuth, checkAdmin, deleteOneForum)
